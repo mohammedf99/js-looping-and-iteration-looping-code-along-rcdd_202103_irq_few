042fd91cffe410fb377ddc6bfb4681b1350f4bc9
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-06 16:10:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function writeCards(names, eventName) {
   
   for (let i = 0; i < names.length; i++) {
     
-    arr[i] = `Thank you, ${names[i]}, for the wonderful birthday gift!`;
+    arr[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
     
     
   }
