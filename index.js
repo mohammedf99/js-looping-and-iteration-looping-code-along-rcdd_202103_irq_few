023481127ce442dab11edcbfd71e826883972095
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-06 16:13:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  
+  let arr = [];
+  
+  for (let i = 0; i < names.length; i++) {
+    
+    arr[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    
+    
+  }
+  
+  return arr;
+  
+}
+
+writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' );
+
+
+function countDown(num) {
+  
+  while(num >= 0){
+    console.log(num);
+    num--;
+  }
+  
+}
+
+countDown(10);
